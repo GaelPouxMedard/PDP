f0b52f1ff9e07bbe148453527714ee8134a0cbed
GaelPouxMedard <61467946+GaelPouxMedard@users.noreply.github.com>
2021-12-04 14:43:45 +0100

    Changes

--- igmm_rw.py
+++ igmm_rw.py
@@ -62,7 +62,7 @@ for nameDS, DS in listDS:
 
     printLogs = True
     plotRes = False
-    allr = [1., 0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1., 1.1]
+    allr = [1., 0.8, 0.9, 1., 1.1]  # 0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7,
     K = 10
     logger = logging.getLogger(__name__)
     logging.basicConfig(level=logging.INFO)

4e3d566bee72d76dea9447a03b3f246e54755ba6
GaelPouxMedard <61467946+GaelPouxMedard@users.noreply.github.com>
2021-12-07 13:27:12 +0100

    Changes

--- LDA/ComputeResults.py
+++ LDA/ComputeResults.py
@@ -2,8 +2,7 @@ import os
 import numpy as np
 from scipy.stats import sem
 
-
-listRes = sorted(os.listdir("Results"))
+listRes = sorted(os.listdir("Results500"))
 
 fileoutputmean = open("Results_mean.txt", "a+")
 fileoutputstd = open("Results_std.txt", "a+")
@@ -13,10 +12,12 @@ fileoutputstd.truncate(0)
 fileoutputsem.truncate(0)
 
 first = True
+dicRes = {}
 for file in listRes:
     r = float(file[-7:-4])
+    if r not in dicRes: dicRes[r] = []
 
-    with open("Results/"+file, "r") as f:
+    with open("Results500/"+file, "r") as f:
         labels = f"r\t"+f.readline()
         if first:
             fileoutputmean.write(labels)
@@ -26,15 +27,16 @@ for file in listRes:
         tabRes = []
         for line in f:
             line = line.replace("\n", "").split("\t")[:-1]
-            tabRes.append(list(map(float, line)))
+            dicRes[r].append(list(map(float, line)))
 
-        arrmean = np.mean(tabRes, axis=0)
-        arrstd = np.std(tabRes, axis=0)
-        arrsem = sem(tabRes, axis=0)
+for r in dicRes:
+    arrmean = np.mean(dicRes[r], axis=0)
+    arrstd = np.std(dicRes[r], axis=0)
+    arrsem = sem(dicRes[r], axis=0)
 
-        fileoutputmean.write(f"{r}\t"+"\t".join(list(map(str, arrmean)))+"\n")
-        fileoutputstd.write(f"{r}\t"+"\t".join(list(map(str, arrstd)))+"\n")
-        fileoutputsem.write(f"{r}\t"+"\t".join(list(map(str, arrsem)))+"\n")
+    fileoutputmean.write(f"{r}\t"+"\t".join(list(map(str, arrmean)))+"\n")
+    fileoutputstd.write(f"{r}\t"+"\t".join(list(map(str, arrstd)))+"\n")
+    fileoutputsem.write(f"{r}\t"+"\t".join(list(map(str, arrsem)))+"\n")
 fileoutputmean.close()
 fileoutputstd.close()
 fileoutputsem.close()
